32e4c4f086b1680129ee665b1a78ecf1fe0fed4d
Gaurav-Dev24 <subhamkr1995dob@gmail.com>
2023-08-18 00:01:43 +0530

    Quiz app webpage link and readme updated

--- reactPractice/quizApp/quiz/src/App.js
+++ reactPractice/quizApp/quiz/src/App.js
@@ -74,7 +74,7 @@ export default function App() {
         <>
           <div className="question-section">
             <div className="question-count">
-              <span>Question 1</span>/{questions.length}
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
             </div>
             <div className="question-text">
               {questions[currentQuestion].questionText}
