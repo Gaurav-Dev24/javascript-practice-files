28c9e9a37b5464d931128793a9cccd605c5dd87d
Gaurav-Dev24 <subhamkr1995dob@gmail.com>
2023-09-04 14:56:24 +0530

    Todo items delete functionality added

--- reactPractice/todo-app/todo-practice/src/components/TodoList.js
+++ reactPractice/todo-app/todo-practice/src/components/TodoList.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React from "react";
 import Todo from "./Todo";
 
-const TodoList = ({todos}) => {
-    // console.log(todos)
+const TodoList = ({ todos, setTodos }) => {
+  // console.log(todos)
   return (
     <div className="todo-container">
       <ul className="todo-list">
         {
           // using the obj. which was created in form component to map its value with unique keys for each todo.
-            todos.map( todo => (
-                <Todo text={todo.text} key={todo.id}/>
-            ))
+          todos.map((todo) => (
+            <Todo
+              todos={todos}
+              todo={todo}
+              setTodos={setTodos}
+              text={todo.text}
+              key={todo.id}
+            />
+          ))
         }
       </ul>
     </div>
+  );
+};
 
-  )
-}
-
-export default TodoList
+export default TodoList;
