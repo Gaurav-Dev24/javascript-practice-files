34310e307c3b4d32032d9073b4dba46519a1fd7a
Gaurav-Dev24 <subhamkr1995dob@gmail.com>
2023-09-23 15:30:39 +0530

    Navbar created and routes set for shop and cart component

--- reactPractice/react-shopping-cart/shopping-cart/src/App.js
+++ reactPractice/react-shopping-cart/shopping-cart/src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Shop from "./pages/shop/Shop";
+import Cart from "./pages/cart/Cart";
+import "./App.css";
+
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />}/>
+          <Route path="/cart" element={<Cart />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ reactPractice/react-shopping-cart/shopping-cart/src/pages/shop/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div className="shop">
+        <div className="shopTitle">
+            <h1>Dev Tech Shop</h1>
+        </div>
+        <div className="products">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Shop
