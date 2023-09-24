1b887b3cb4d687bb871e4b158f48ef1cf69d273c
Gaurav-Dev24 <subhamkr1995dob@gmail.com>
2023-09-24 09:24:23 +0530

    Products and assests added

---
+++ reactPractice/react-shopping-cart/shopping-cart/src/Products.js
@@ -0,0 +1,81 @@
+import product1 from "./assets/1.jpg";
+import product2 from "./assets/2.jpg";
+import product3 from "./assets/3.jpg";
+import product4 from "./assets/4.jpg";
+import product5 from "./assets/5.jpg";
+import product6 from "./assets/6.jpg";
+import product7 from "./assets/7.jpg";
+import product8 from "./assets/8.jpg";
+import product9 from "./assets/9.jpg";
+import product10 from "./assets/10.jpg";
+import product11 from "./assets/11.jpg";
+
+
+export const PRODUCTS = [
+{
+    id: 1,
+    productName: "Nikkon 550D",
+    price: 1599.0,
+    productImage: product1, 
+},
+{
+    id: 2,
+    productName: "Lorel Lipstick",
+    price: 19.0,
+    productImage: product2, 
+},
+{
+    id: 3,
+    productName: "Mac book Pro 2022 (M1)",
+    price: 4999.0,
+    productImage: product3, 
+},
+{
+    id: 4,
+    productName: "Rolex Faze 204",
+    price: 2999.0,
+    productImage: product4, 
+},
+{
+    id: 5,
+    productName: "Sony Mic & Headphone Combo",
+    price: 799.0,
+    productImage: product5, 
+},
+{
+    id: 6,
+    productName: "Gucci Skirt",
+    price: 499.0,
+    productImage: product6, 
+},
+{
+    id: 7,
+    productName: "IPhone 14 Max pro",
+    price: 999.0,
+    productImage: product7, 
+},
+{
+    id: 8,
+    productName: "Two Shirts & Formal Shoes Combo",
+    price: 399.0,
+    productImage: product8, 
+},
+{
+    id: 9,
+    productName: "IPhone Holder",
+    price: 79.0,
+    productImage: product9, 
+},
+{
+    id: 10,
+    productName: " Boat Headphone",
+    price: 199.0,
+    productImage: product10, 
+},
+{
+    id: 11,
+    productName: "Spectacles",
+    price: 49.0,
+    productImage: product11, 
+},
+];
