2252af54b7da097bc6697c6f7e4ff1ab98dfd0b2
Gaurav-Dev24 <subhamkr1995dob@gmail.com>
2023-09-28 20:34:39 +0530

    Shopping cart completed with all the functionality

--- reactPractice/react-shopping-cart/shopping-cart/src/context/ShopContext.js
+++ reactPractice/react-shopping-cart/shopping-cart/src/context/ShopContext.js
@@ -1,40 +1,61 @@
-import React, { createContext, useState } from 'react';
-import PRODUCTS from '../Products';
+import React, { createContext, useState } from "react";
+import PRODUCTS from "../Products";
 
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
-    let cart = {};
-    for (let i = 1; i < PRODUCTS.length + 1; i++) {
-        cart[i] = 0;
-    }
-    return cart;
-}
+  let cart = {};
+  for (let i = 1; i < PRODUCTS.length + 1; i++) {
+    cart[i] = 0;
+  }
+  return cart;
+};
 
 const ShopContextProvider = (props) => {
-    const [cartItems, setCartItems] = useState(getDefaultCart());
+  const [cartItems, setCartItems] = useState(getDefaultCart());
 
-    // handler function to add items to cart
-    const addToCart = (itemId) => {
-        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1} ));
-    };
+  // handler function to add items to cart
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+  };
 
-    // handler function to remove from cart
-    const removeFromCart = (itemId) => {
-        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] - 1} ));
-    };
+  // handler function to remove from cart
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
 
-    // handler function to update value of cartItems 
-    const updateCartItemCount = (newAmount, itemId) => {
-        setCartItems((prev) => ({...prev, [itemId]: newAmount}));
+  // handler function to update value of cartItems
+  const updateCartItemCount = (newAmount, itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: newAmount }));
+  };
+
+  // handler function to get the total price of each items
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        // get the price of each item in cart
+        let itemInfo = PRODUCTS.find((product) => product.id === Number(item));
+        totalAmount += cartItems[item] * itemInfo.price;
+      }
     }
+    return totalAmount;
+  };
 
-    // creating a variable object to pass values to context
-    const contextValue = {cartItems, addToCart, removeFromCart, updateCartItemCount};
-    console.log(cartItems)
+  // creating a variable object to pass values to context
+  const contextValue = {
+    cartItems,
+    addToCart,
+    removeFromCart,
+    updateCartItemCount,
+    getTotalCartAmount,
+  };
+//   console.log(cartItems);
   return (
-    <ShopContext.Provider value={contextValue}>{props.children}</ShopContext.Provider>
-    );
-}
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
 
 export default ShopContextProvider;
--- reactPractice/react-shopping-cart/shopping-cart/src/pages/cart/Cart.js
+++ reactPractice/react-shopping-cart/shopping-cart/src/pages/cart/Cart.js
@@ -1,27 +1,43 @@
-import React, { useContext } from 'react';
-import PRODUCTS from '../../Products';
-import { ShopContext } from '../../context/ShopContext';
-import CartItem from './CartItem';
+import React, { useContext } from "react";
+import PRODUCTS from "../../Products";
+import { ShopContext } from "../../context/ShopContext";
+import CartItem from "./CartItem";
+import { useNavigate } from "react-router-dom";
 import "./Cart.css";
 
 const Cart = () => {
   // getting cartItems from context
-  const {cartItems} = useContext(ShopContext)
+  const { cartItems, getTotalCartAmount } = useContext(ShopContext);
+
+  // creating a variable for total amount
+  let totalAmount = getTotalCartAmount();
+
+  let navigate = useNavigate();
   return (
-    <div className='cart'>
+    <div className="cart">
       <div>
         <h1>Your Cart Items</h1>
       </div>
       <div className="cartItems">
-          {PRODUCTS.map((product) => {
-            if(cartItems[product.id] !==0 ){
-              return <CartItem data={product} /> // passing the data prop to CartItem component
-            }
-            
-          })}
+        {PRODUCTS.map((product) => {
+          if (cartItems[product.id] !== 0) {
+            return <CartItem data={product} />; // passing the data prop to CartItem component
+          }
+        })}
       </div>
+      {/* If the cart has any items then it will show */}
+      {totalAmount > 0 ? (
+        <div className="checkout">
+          <p>Subtotal: $ {totalAmount} </p>
+          <button onClick={() => navigate("/")}>Continue Shopping</button>
+          <button>Checkout</button>
+        </div>
+      ) : (
+        // Else cart is empty
+        <h1>Your Cart is Empty</h1>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
